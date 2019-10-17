7e5653bb3adb47751611ff11d79e3ae385a64efc
Johnbastian <johnbastian@tray.io>
2019-10-17 22:11:10 +0100

    Global tests updated

--- tests/global_test.js
+++ tests/global_test.js
@@ -1,22 +1,40 @@
-var assert       = require('assert');
-var _            = require('lodash');
-var ThreadNeedle = require('../');
+const assert = require('assert');
+const _ = require('lodash');
+const ThreadNeedle = require('../');
 
 describe('Global settings', function () {
 
-  describe('#global', function () {
+	describe.only('#global', function () {
 
-    it('should set the global settings', function () {
-      var threadneedle = new ThreadNeedle();
-      threadneedle.global({
-        chris: 'test'
-      });
-      assert.deepEqual(threadneedle._globalOptions, { 
-        chris: 'test'
-      });
-    });
+		it('type should be `rest` by default', function () {
+			const threadneedle = new ThreadNeedle();
+			assert.deepEqual(threadneedle._globalOptions, {
+				type: 'rest'
+			});
+		});
 
-  });
+		it('should set the global settings', function () {
+			const threadneedle = new ThreadNeedle();
+			threadneedle.global({
+				chris: 'test'
+			});
+			assert.deepEqual(threadneedle._globalOptions, {
+				type: 'rest',
+				chris: 'test'
+			});
+		});
+
+		it('should override default if `type` is defined and valid', function () {
+			const threadneedle = new ThreadNeedle();
+			threadneedle.global({
+				type: 'SOAP'
+			});
+			assert.deepEqual(threadneedle._globalOptions, {
+				type: 'soap'
+			});
+		});
+
+	});
 
 
 });
