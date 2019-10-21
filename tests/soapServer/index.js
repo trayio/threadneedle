befc813142812c4043ed8b75df94fdcaee7e8629
Johnbastian <johnbastian@tray.io>
2019-10-21 22:43:10 +0100

    more soap server WIP

--- tests/soapServer/index.js
+++ tests/soapServer/index.js
@@ -1,12 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 
+const _ = require('lodash');
+const needle = require('needle');
 const SOAP = require('soap');
 
 // SOAP.createClient(__dirname + '/regonline.wsdl', (err, client) => {
 // 	console.log(__dirname);
 // 	console.log(err);
-// 	console.log(require('util').inspect(client.describe(), { depth: null }));
+// 	const clientDescription = client.describe();
+// 	fs.writeFileSync(__dirname + '/describe.json', JSON.stringify(clientDescription, null, '\t'), 'utf8');
 // });
 
 const soapService = require('./soapService');
@@ -19,13 +22,36 @@ const server = http.createServer(function (request, response) {
 	response.end('404: Not Found: ' + request.url);
 });
 
-server.listen(8000);
-SOAP.listen(server, '/wsdl', soapService, wsdlXML, function () {
-	console.log('server initialized');
+const soapServer = SOAP.listen(server, '/wsdl', soapService, wsdlXML);
+
+soapServer.on('request', (req, methodName) => {
+	console.log('reqest', methodName);
 });
 
-const needle = require('needle');
+soapServer.on('response', (resp, methodName) => {
+	console.log('response', methodName);
+	console.log(resp);
+});
 
-needle.get('localhost:8000/wsdl', (err, res, data) => {
-	console.log(data.toString());
+
+const request = {
+	headers: {
+		'Content-Type': 'text/xml; charset=utf-8',
+		SOAPAction: '"http://www.regonline.com/api/GetEvents"'
+	},
+	body: '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xmlns:tm="http://microsoft.com/wsdl/mime/textMatching/" xmlns:tns="http://www.regonline.com/api"><soap:Header><TokenHeader xmlns:undefined="http://www.regonline.com/api" xmlns="http://www.regonline.com/api"><APIToken>lO29j0in23WRCF9s3b6LvqARu1FCIhohPTVP4Pu1yom2y2h005KRAQ==</APIToken></TokenHeader></soap:Header><soap:Body><GetEvents xmlns="http://www.regonline.com/api"><orderBy>ID DESC</orderBy></GetEvents></soap:Body></soap:Envelope>'
+};
+
+server.listen(8000, null, null, () => {
+	console.log('server initialized');
+	console.log(_.keys(soapServer));
+	console.log(soapServer.path);
+	console.log(soapServer.services);
+
+	needle.post('localhost:8000/wsdl', request.body, { headers: request.headers }, (err, res, data) => {
+		console.log('res');
+		// console.log(res.statusCode);
+		console.log(res.body);
+		// console.log(data.toString());
+	});
 });
