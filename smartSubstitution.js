7acceec2c147cc22ae263957c626d494974d29ea
Johnbastian <johnbastian@tray.io>
2020-01-20 12:55:50 +0000

    Added alias for smartSubstitution on top level

---
+++ smartSubstitution.js
@@ -0,0 +1,5 @@
+/*
+	Top level alias for exposing substitue.js
+	This enables a lib to simply do `require('@trayio/threadneedle/smartSubstitution')`
+*/
+module.exports = require('./lib/addMethod/substitute.js');
