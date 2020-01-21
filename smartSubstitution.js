86a411263260c0c1c8fcd02692b3ff1670ab0cef
Johnbastian <johnbastian@tray.io>
2020-01-21 11:37:12 +0000

    Added validation for params

--- smartSubstitution.js
+++ smartSubstitution.js
@@ -1,5 +1,14 @@
 /*
-	Top level alias for exposing substitue.js
+	Top level alias for exposing substitute.js
 	This enables a lib to simply do `require('@trayio/threadneedle/smartSubstitution')`
 */
-module.exports = require('./lib/addMethod/substitute.js');
+const _ = require('lodash');
+
+const substitute = require('./lib/addMethod/substitute.js');
+
+module.exports = (target, params = {}) => {
+	if (!_.isPlainObject(params)) {
+		throw new Error('`params` must be an object');
+	}
+	return substitute(target, params);
+};
